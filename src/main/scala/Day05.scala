c657b827380a59b1550d71c9c13233cd479a5e64
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2023-09-24 14:05:27 -0400

    Solve Day 5 Part 2

--- src/main/scala/Day05.scala
+++ src/main/scala/Day05.scala
@@ -119,6 +119,8 @@ object Day05 {
         println(s"Part 2: TBD")
         val p2T0 = Instant.now()
 
+        // at least 1 char that appears twice in a row
+        val pattern4 = "(a{2}|b{2}|c{2}|d{2}|e{2}|f{2}|g{2}|h{2}|i{2}|j{2}|k{2}|l{2}|m{2}|n{2}|o{2}|p{2}|q{2}|r{2}|s{2}|t{2}|u{2}|v{2}|w{2}|x{2}|y{2}|z{2})".r // use findAllIn and count >= 1
 
 
         val delta2 = Duration.between(p2T0, Instant.now())
